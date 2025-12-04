953c462fcedfa8d4309e3f30d06fb91562887648
diniscmadeira <dmadeira1994@gmail.com>
2025-12-04 14:11:34 +0000

    Enhance motor control with PWM and speed variable
    Added PWM pins and global motor speed variable for motor control.

--- motores.h
+++ motores.h
@@ -1,10 +1,18 @@
-----------MOTORES.H----------                           #pragma once
+#pragma once
+#include <Arduino.h>
 
-// Pinos reservados para o driver/ponte H (ajusta ao teu módulo)
+// Pinos da ponte H (ajusta se necessário)
 const int PIN_MOTOR_IN1 = 25;
 const int PIN_MOTOR_IN2 = 26;
 const int PIN_MOTOR_IN3 = 27;
 const int PIN_MOTOR_IN4 = 14;
+
+// Pinos PWM (ENA / ENB do driver de motor)
+const int PIN_ENA = 32;   // motor A (IN1, IN2)
+const int PIN_ENB = 33;   // motor B (IN3, IN4)
+
+// Velocidade global (0–255)
+int motorSpeed = 80;      // baixa velocidade para testes
 
 void setupMotores() {
   pinMode(PIN_MOTOR_IN1, OUTPUT);
@@ -12,7 +20,12 @@ void setupMotores() {
   pinMode(PIN_MOTOR_IN3, OUTPUT);
   pinMode(PIN_MOTOR_IN4, OUTPUT);
 
+  pinMode(PIN_ENA, OUTPUT);
+  pinMode(PIN_ENB, OUTPUT);
+
   // Começa parado
+  analogWrite(PIN_ENA, 0);
+  analogWrite(PIN_ENB, 0);
   digitalWrite(PIN_MOTOR_IN1, LOW);
   digitalWrite(PIN_MOTOR_IN2, LOW);
   digitalWrite(PIN_MOTOR_IN3, LOW);
@@ -20,18 +33,22 @@ void setupMotores() {
 }
 
 void parar() {
+  analogWrite(PIN_ENA, 0);
+  analogWrite(PIN_ENB, 0);
   digitalWrite(PIN_MOTOR_IN1, LOW);
   digitalWrite(PIN_MOTOR_IN2, LOW);
   digitalWrite(PIN_MOTOR_IN3, LOW);
   digitalWrite(PIN_MOTOR_IN4, LOW);
 }
 
-// Exemplos genéricos: adapta à tua ponte (L298N, L9110, etc.)
 void andarFrente() {
   digitalWrite(PIN_MOTOR_IN1, HIGH);
   digitalWrite(PIN_MOTOR_IN2, LOW);
   digitalWrite(PIN_MOTOR_IN3, HIGH);
   digitalWrite(PIN_MOTOR_IN4, LOW);
+
+  analogWrite(PIN_ENA, motorSpeed);
+  analogWrite(PIN_ENB, motorSpeed);
 }
 
 void andarTras() {
@@ -39,6 +56,9 @@ void andarTras() {
   digitalWrite(PIN_MOTOR_IN2, HIGH);
   digitalWrite(PIN_MOTOR_IN3, LOW);
   digitalWrite(PIN_MOTOR_IN4, HIGH);
+
+  analogWrite(PIN_ENA, motorSpeed);
+  analogWrite(PIN_ENB, motorSpeed);
 }
 
 void rodarEsquerda() {
@@ -46,6 +66,9 @@ void rodarEsquerda() {
   digitalWrite(PIN_MOTOR_IN2, HIGH);
   digitalWrite(PIN_MOTOR_IN3, HIGH);
   digitalWrite(PIN_MOTOR_IN4, LOW);
+
+  analogWrite(PIN_ENA, motorSpeed);
+  analogWrite(PIN_ENB, motorSpeed);
 }
 
 void rodarDireita() {
@@ -53,4 +76,7 @@ void rodarDireita() {
   digitalWrite(PIN_MOTOR_IN2, LOW);
   digitalWrite(PIN_MOTOR_IN3, LOW);
   digitalWrite(PIN_MOTOR_IN4, HIGH);
+
+  analogWrite(PIN_ENA, motorSpeed);
+  analogWrite(PIN_ENB, motorSpeed);
 }
